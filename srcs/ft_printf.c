b7747ca61d239b9db60a67b9837f40c9a4e69681
Ranjan mallawarachchi <96380657+Ranjan2012@users.noreply.github.com>
2022-02-10 22:20:00 +1030

    Add files via upload
    first commit for printf/srcs

---
+++ srcs/ft_printf.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmallawa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/30 15:03:00 by rmallawa          #+#    #+#             */
+/*   Updated: 2022/02/10 21:17:09 by rmallawa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static t_print	*start(void)
+{
+	t_print	*arg_count;
+
+	arg_count = (t_print *)malloc(sizeof(t_print));
+	if (arg_count == NULL)
+		return (NULL);
+	arg_count->counter = 0;
+	return (arg_count);
+}
+
+void	printcheck(const char character, t_print *arg_count)
+{
+	if (character == 'c')
+		ifcharacter(arg_count);
+	else if (character == 's')
+		ifstring(arg_count);
+	else if (character == 'p')
+		ifpointer(arg_count);
+	else if (character == 'i' || character == 'd')
+		ifint(arg_count);
+	else if (character == 'u')
+		ifunsignedint(arg_count);
+	else if (character == 'x' || character == 'X')
+		ifhex(arg_count, character);
+	else if (character == '%')
+	{
+		ft_putchar_fd('%', 1);
+		arg_count->counter++;
+	}
+	else if (character != '\0')
+	{
+		write(1, &character, 1);
+		arg_count->counter++;
+	}
+}
+
+int	ft_printf(const char *string, ...)
+{
+	t_print	*arg_count;
+	int		printcomplete;
+	int		i;
+
+	i = 0;
+	arg_count = start();
+	printcomplete = 0;
+	va_start(arg_count->args, string);
+	while (string[i] != '\0')
+	{
+		if (string[i] == '%')
+			printcheck(string[++i], arg_count);
+		else
+		{
+			ft_putchar_fd(string[i], 1);
+			arg_count->counter++;
+		}
+		if (string[i] != '\0')
+			i++;
+	}
+	printcomplete = arg_count->counter;
+	va_end(arg_count->args);
+	free(arg_count);
+	return (printcomplete);
+}
